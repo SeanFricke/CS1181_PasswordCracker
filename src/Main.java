7276fda3e278e8f2651f9b4566f11966d9025ebb
Sean Fricke <seanfricke03@gmail.com>
2024-12-11 16:49:29 -0500

    Cleaned up main file

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-import java.util.ArrayList;
-
 public class Main {
-    public static final String zipPath = "protected3"; // Zip file name without extension
-    public static final int numThreads = 30;
+    public static final String ZIP_PATH = "protected3";// Zip file name without extension
+    public static final int PASSWORD_LENGTH = 3; // Length of password that will be cracked
+    public static final int NUM_THREADS = 5; // Number of threads to crack with
 
     public static void main(String[] args) throws InterruptedException {
-//        Cracker protect3 = new Cracker(zipPath, 3);
-//        protect3.crack(numThreads);
-        Cracker protect5 = new Cracker("protected5", 5);
-        protect5.crack(numThreads);
+        // TODO take out /tmp/ file dirs when extracting.
+        long startTime, endTime;
+        double timeDelta;
 
-
+        Cracker zipCrack = new Cracker(ZIP_PATH, PASSWORD_LENGTH);
+        startTime = System.currentTimeMillis();
+        zipCrack.crack(NUM_THREADS);
+        endTime = System.currentTimeMillis();
+        timeDelta = (endTime - startTime) / 100.0;
+        System.out.println(timeDelta);
     }
 }
